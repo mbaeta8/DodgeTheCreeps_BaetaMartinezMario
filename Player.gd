1662c0aa0f08df45ff0c5d76c8d65e5cefb04f53
mbaeta8 <mbaeta8@gmail.com>
2024-10-18 15:12:12 +0200

    Assignació de tecles

--- Player.gd
+++ Player.gd
@@ -3,12 +3,5 @@ extends Area2D
 export var speed = 400 # A quina velocitat es moura el jugador (pixels/seg).
 var screen_size # Mida de la finestra de joc.
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	screen_size = get_viewport_rect().size
