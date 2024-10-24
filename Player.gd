6afefa55be43fdd211d8dfc5e3044264ba8ed3f0
mbaeta8 <mbaeta8@gmail.com>
2024-10-24 16:43:12 +0200

    Animacions jugador.

--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ var screen_size # Mida de la finestra de joc.
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO # Vector de moviment del jugador.
@@ -22,6 +23,14 @@ func _process(delta):
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
+
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
 
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
